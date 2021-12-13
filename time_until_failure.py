80157e63308ba251f5cd439ff3f72e2a3d2f9e2d
Pranav Gokhale <pranav@super.tech>
2021-12-13 17:10:22 -0600

    time until failure results

---
+++ time_until_failure.py
@@ -0,0 +1,15 @@
+import numpy as np
+import stim
+
+from games.stabilizer2_0 import StabilizerEnv
+
+for n in range(2, 10):
+    num_moves_list = []
+    for _ in range(500):
+        env = StabilizerEnv(n); env.reset()
+        num_moves = 0
+        while env.step(np.random.choice([0, 1]))[2] is False:
+            num_moves += 1
+        num_moves_list.append(num_moves)
+
+    print(n, np.mean(num_moves_list))
